e3b931c15cde10cc9e49c1630577edfa6d6778b8
zivserphos <zivfromisrael@gmail.com>
2022-01-23 19:27:01 +0200

    2

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import serverless from "serverless-http";
-import config from "./utils/index";
 import app from "./app";
-
-const PORT = config.port || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
 
 const handler = serverless(app);
 
